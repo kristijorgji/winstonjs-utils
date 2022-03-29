7224ebeb22e96d5a01cecbe294efb36ca74e1e1b
kristijorgji <kristijorgji@hotmail.com>
2022-03-29 13:22:03 +0200

    fix(winstonjs-fingers-crossed-transport, winstonjs-html-format, winstonjs-mail-transport): relative symlink

--- dev/link.sh
+++ dev/link.sh
@@ -7,18 +7,18 @@ This script will link everything under _common to every subproject (package)
 This is avoid having to change same files in every package, instead can change the real file at _common
 and all soft links will be updated automatically
 
+It will use relative paths in order to be able to reproduce results in any machine
 ////
 
 script_directory="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${script_directory}/common.sh"
 
-workdir=$(pwd)
-commonFilesPath=$(abspath "$workdir/_common")
-
-for commonItem in $commonFilesPath/* ; do
+commonFilesRelPath="_common"
+echo $commonFilesRelPath
+for commonItem in $commonFilesRelPath/* ; do
     for d in packages/*/ ; do
-        absProjectPath=$(abspath $d)
-        ln -s "$commonItem" "$absProjectPath/"
-        echo -e "soft linked $commonItem to $absProjectPath\n"
+        commonItemRelToPackagePath="../../$commonItem"
+        ln -sf "$commonItemRelToPackagePath" "$d/"
+        echo -e "soft linked $commonItemRelToPackagePath to $d\n"
     done
 done
